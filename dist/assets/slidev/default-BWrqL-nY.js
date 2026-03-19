2cf65a175f6233e629b5e46beaf3802206a19396
Willow Sparks <willow.sparks002@gmail.com>
2026-03-19 19:05:30 +0000

    Remove dist/ from repo, add to gitignore

--- dist/assets/slidev/default-BWrqL-nY.js
+++
@@ -1,1 +0,0 @@
-import{o,e as t,i as s}from"../modules/vue-BVwtu4a7.js";import{_ as r}from"../index-DF3UTnWv.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
